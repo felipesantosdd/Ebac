75d57f31299709aac2d7603ce5c3b99da96a59e8
Felipe <felipesantosdd@gmail.com>
2023-11-19 15:50:57 -0300

    feat: Exercicio do modulo 7.

---
+++ src/Calculator.java
@@ -0,0 +1,71 @@
+package src;
+
+// Classe que representa uma calculadora simples
+public class Calculator {
+    // Variáveis para armazenar os dois números
+    private int num1 = 0;
+    private int num2 = 0;
+
+    // Método principal que cria uma instância da calculadora e executa as operações
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.teste(args);
+    }
+
+    // Método de execução que recebe argumentos, converte para inteiros e realiza
+    // operações
+    private void teste(String[] args) {
+        try {
+            num1 = Integer.parseInt(args[0]);
+            num2 = Integer.parseInt(args[1]);
+            Soma();
+            Subtracao();
+            Multiplicacao();
+            Divisao();
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+            System.out.println("Os números fornecidos não são válidos.");
+        }
+    }
+
+    // Método de soma, recebe dois valores como argumentos e imprime o resultado
+    public void Soma() {
+        try {
+            int result = num1 + num2;
+            System.out.println("A soma de " + num1 + " e " + num2 + " é igual a " + result);
+        } catch (Exception e) {
+            System.out.println("Erro ao calcular a soma.");
+        }
+    }
+
+    // Método de subtração, recebe dois valores como argumentos e imprime o
+    // resultado
+    public void Subtracao() {
+        try {
+            int result = num1 - num2;
+            System.out.println("A subtração de " + num1 + " por " + num2 + " é igual a " + result);
+        } catch (Exception e) {
+            System.out.println("Erro ao calcular a subtração.");
+        }
+    }
+
+    // Método de multiplicação, recebe dois valores como argumentos e imprime o
+    // resultado
+    public void Multiplicacao() {
+        try {
+            int result = num1 * num2;
+            System.out.println("A multiplicação de " + num1 + " por " + num2 + " é igual a " + result);
+        } catch (Exception e) {
+            System.out.println("Erro ao calcular a multiplicação.");
+        }
+    }
+
+    // Método de divisão, recebe dois valores como argumentos e imprime o resultado
+    public void Divisao() {
+        try {
+            int result = num1 / num2;
+            System.out.println("A divisão de " + num1 + " por " + num2 + " é igual a " + result);
+        } catch (Exception e) {
+            System.out.println("Erro ao calcular a divisão.");
+        }
+    }
+}
